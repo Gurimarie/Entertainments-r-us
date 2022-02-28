8b77d91190da215149251108e156cdb2cea43635
Gurimarie <gurimarie@gmail.com>
2022-02-28 05:40:36 +0000

    Fix Django secret key (create new and hide in .env)

--- entertainments_r_us/settings.py
+++ entertainments_r_us/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5x2ht8w&7dus^o_82nhby7f5dqh0+s)8osz2!+7buwb=kahtn!'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
