b706920531c7e2b0aec630d43dbf855b3649f742
Gurimarie <gurimarie@gmail.com>
2022-04-30 09:41:21 +0000

    Add DATABASE-options

--- entertainments_r_us/settings.py
+++ entertainments_r_us/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = [
+    'https://entertainments-r-us.herokuapp.com/', 
+    'localhost'
+]
 
 
 # Application definition
