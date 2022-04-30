7bcdb7cf1a786d23d9b3589a4790a2500aca0b8b
Gurimarie <gurimarie@gmail.com>
2022-04-30 06:41:21 +0000

    Add views and html

--- performances/views.py
+++ performances/views.py
@@ -1,3 +1,17 @@
+""" Docstring """
 from django.shortcuts import render
+from .models import Performances
 
 # Create your views here.
+
+
+def all_performances(request):
+    """ A view to return the all_performances, including sorting and searching """
+
+    performances = Performances.objects.all()
+
+    context = {
+        'performances': performances,
+    }
+    
+    return render(request, 'performances/performances.html', context)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
+""" Docstring """
 from django.shortcuts import render
+from .models import Products
 
 # Create your views here.
+
+
+def all_products(request):
+    """ A view to return the all_products-page, + sorting and searching """
+
+    products = Products.objects.all()
+
+    context = {
+        'products': products,
+    }
+    
+    return render(request, 'products/products.html', context)
+
