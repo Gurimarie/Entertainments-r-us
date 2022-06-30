72912c2c50e6f3bd5ad9d17c6be462696248c67b
Gurimarie <gurimarie@gmail.com>
2022-06-30 17:58:37 +0000

    Import Lower-function

--- performances/views.py
+++ performances/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Performance, Artist, Category, ArtistType
 
 
