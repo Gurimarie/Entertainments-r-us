664acd93024e7ca840dbb2430d258647c04aea35
Gurimarie <gurimarie@gmail.com>
2022-07-08 18:52:26 +0000

    Add class methods to wStripe wehook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -10,5 +10,17 @@ class StripeWH_Handler:
     def handle_event(self, event):
         """ handle generic/unknown/unexpected webhook-event """
         return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """ handle payment_intent.succeeded webhook-event from Stripe """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """ handle payment_intent.failed webhook-event from Stripe """
+        return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
--- entertainments_r_us/settings.py
+++ entertainments_r_us/settings.py
@@ -199,3 +199,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
