c1b14ff02714f7e3dc0f231d3b88a03efe0f4d42
Gurimarie <gurimarie@gmail.com>
2022-07-09 13:00:33 +0000

    Update allauth-templates and test profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
+
 
 def customer_profile(request):
     """ display the users profile """
+    customer_profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/customer_profile.html'
-    context = {}
+    context = {
+        'customer_profile': customer_profile,
+    }
 
     return render(request, template, context)
